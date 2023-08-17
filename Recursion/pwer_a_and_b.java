6d42cfb07613a4ffa03aee1b6dd47f2df857fe3b
Sushil Rakesh Chaubey <125786068+Sushildevelop@users.noreply.github.com>
2023-08-17 23:43:02 +0530

    Add files via upload

--- Recursion/pwer_a_and_b.java
+++ Recursion/pwer_a_and_b.java
@@ -11,10 +11,25 @@ public class pwer_a_and_b {
         return ans;
 
     }
+    // Efficient Approach
+    static int Power(int a , int b){
+       if (b==0){
+           return 1;
+       }
+       if (b%2==0){
+           int result=Power(a,b/2);
+           return result*result;
+       }
+       else {
+           int result=Power(a,(b-1)/2);
+           return a*result*result;
+       }
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        System.out.println(power(a,b));
+    //    System.out.println(power(a,b));
+        System.out.println(Power(a,b));
     }
 }
